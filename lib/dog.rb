d8404b0c902db9c15f6a2d59a4b17dc29b4896ab
Olivia Kesler <oakesler@protonmail.ch>
2019-12-12 19:09:09 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,11 @@
-class Dog
+class Dog < ActiveRecord::Base
+  attr_accessor :id, :name, :breed
+  
+  def initialize(id: "nil", name: , breed: )
+    @id = id 
+    @name = name
+    @breed = breed
+  end
+  
+  
 end
