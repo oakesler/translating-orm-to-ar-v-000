9ecc5b1127e1a976e89214a3bbd99d101610d939
Olivia Kesler <oakesler@protonmail.ch>
2019-12-12 19:22:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,8 +12,9 @@ class Dog < ActiveRecord::Base
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
   end
   
-  def self.create(:name , :breed )
-    
+  def self.create(name: , breed: )
+    self.create(name: ,breed: )
+  end
     
 end
   #def self.create()
