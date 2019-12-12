26ed6590abd96a0dd1a0c2e183738edb9daad108
Olivia Kesler <oakesler@protonmail.ch>
2019-12-12 19:19:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,22 +7,26 @@ class Dog < ActiveRecord::Base
     @breed = breed
   end
   
-  def self.create()
-    
- 
+  def self.save
+    self.save
   end
   
-  def save
-    if self.id
-      self.update
-    else
-      sql = <<-SQL
-      INSERT INTO students (name, grade)
-      VALUES (?, ?)
-      SQL
-      DB[:conn].execute(sql, self.name, self.grade)
-      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-  end
+  #def self.create()
+    
+ 
+  #end
+  
+  #def save
+    #if self.id
+      #self.update
+    #else
+      #sql = <<-SQL
+      #INSERT INTO students (name, grade)
+      #VALUES (?, ?)
+      #SQL
+      #DB[:conn].execute(sql, self.name, self.grade)
+      #@id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  #end
 end
   
   
