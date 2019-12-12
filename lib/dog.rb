12e690eb4d5e9a21595591c1fdc716b035a18589
Olivia Kesler <oakesler@protonmail.ch>
2019-12-12 19:32:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -13,26 +13,33 @@ class Dog < ActiveRecord::Base
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
   end
   
-  def self.create(name: , breed: )
+  def self.create(input = {})
     self.create(name, breed)
   end
   
-  def self.find_or_create_by
-end
-
-
-def self.find_or_create_by(name: , breed: )
-     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
-    if !dog.empty?
-      dog_data = dog[0]
-      dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
-    else
-      dog = self.create(name: name, breed: breed)
+  def self.find_or_create_by(input = {})
+    if !self.find_by(input[0])
+      dog = self.create(input)
+    else 
+      dog = self.find_by(input[0])
     end
     dog
   end
+d
+
+
+#def self.find_or_create_by(name: , breed: )
+     #dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+    #if !dog.empty?
+      #dog_data = dog[0]
+      #dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+    #else
+      #dog = self.create(name: name, breed: breed)
+    #end
+    #dog
+  #end
     
-end
+#end
   #def self.create()
     
  
